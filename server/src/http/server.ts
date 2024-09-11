adffb9cf8e092deb94b9f91d1bd515e395b257aa
livehass <silva.felipe12@hotmail.com>
2024-09-11 11:25:05 -0300

    added routes functions

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,11 +1,66 @@
-import fastfy from "fastify";
+import fastfy from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createGoalCompletion } from '../functions/create-goal-completion'
 
-const app = fastfy();
+const app = fastfy().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.get('/penging-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { title, desiredWeeklyFrequency } = request.body
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+
+    const result = await createGoalCompletion({
+      goalId,
+    })
+  }
+)
 
 app
-	.listen({
-		port: 3333,
-	})
-	.then(() => {
-		console.log("HTTP server running!");
-	});
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running!')
+  })
