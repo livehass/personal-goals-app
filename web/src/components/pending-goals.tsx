f3d22813661c9547b3560f62398a1128126e44f3
livehass <silva.felipe12@hotmail.com>
2024-09-12 11:09:30 -0300

    added new component pendinggoals

---
+++ web/src/components/pending-goals.tsx
@@ -0,0 +1,38 @@
+import { Plus } from 'lucide-react'
+import { OutlineButton } from './ui/outline-button'
+import { getPendingGoals } from '../http/get-pending-goals'
+import { useQuery } from '@tanstack/react-query'
+import { CreateGoalCompletion } from '../http/create-goal-completion'
+
+export function PendingGoals() {
+  const { data } = useQuery({
+    queryKey: ['pending-goals'],
+    queryFn: getPendingGoals,
+    staleTime: 1000 * 60, // 60 seconds in this case optinal
+  })
+
+  if (!data) {
+    return null
+  }
+
+  async function handleCompleteGoal(goalId: string) {
+    await CreateGoalCompletion(goalId)
+  }
+
+  return (
+    <div className="flex flex-wrap gap-3">
+      {data.map(goal => {
+        return (
+          <OutlineButton
+            key={goal.id}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
+            onClick={() => handleCompleteGoal(goal.id)}
+          >
+            <Plus className="size-4 text-zinc-600" />
+            {goal.title}
+          </OutlineButton>
+        )
+      })}
+    </div>
+  )
+}
